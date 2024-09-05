4e86952296b2400bbec7422b55afa46bca29cb5a
gptcs1 <sanjuganiga9@gmail.com>
2024-09-05 15:32:03 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,20 @@
+ages = {
+
+‘hannah’: 21,
+
+‘james’: 21
+
+‘jonathan’: 41
+
+}
+
+print(f’Jonathan is {ages[“jonathan”]} years old.’)
+
+When you run this code, you will get the following traceback:
+
+$ python gamedev.py
+
+File “gamedev.py”, line 3
+
+‘jonathan’: 41
+
